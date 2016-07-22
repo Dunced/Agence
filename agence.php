c8dd5d79876cdf818fd2cbcaf502a5ff0cbdd8db
Dunced <fresh_lemona@yahoo.fr>
2016-07-22 16:22:19 +0200

    Add files via upload

--- agence.php
+++ agence.php
@@ -1,10 +1,10 @@
 <?php
-  session_start(); // démarrage de la session
-  // obligatoire pour récupérer les variables de session stockées
-  $_SESSION['civilite'] = $_POST['civilite'];
-  $_SESSION['nom'] = $_POST['nom'];
-  $_SESSION['prenom'] = $_POST['prenom'];
-  $_SESSION['voyage-excursion'] = $_POST['voyage-excursion'];
+session_start(); // démarrage de la session
+// obligatoire pour récupérer les variables de session stockées
+$_SESSION['civilite'] = $_POST['civilite'];
+$_SESSION['nom'] = $_POST['nom'];
+$_SESSION['prenom'] = $_POST['prenom'];
+$_SESSION['voyage-excursion'] = $_POST['voyage-excursion'];
 ?>
 <!DOCTYPE html>
 <html>
@@ -40,7 +40,7 @@
 
     <!-- jquery libs -->
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    <title>Choisissez votre destination</title>
+        <title>Choisissez votre destination</title>
 
   </head>
   <body>
@@ -52,40 +52,44 @@
     <div class="containerpanel panel-default col-md-6 col-md-offset-3">
       <!-- Default panel contents -->
       <div class="panel-heading bg-panel-heading text-center">Choisissez votre destination</div>
-      <div class="panel-body">
-        <?php
-          if ($_SESSION['voyage-excursion'] == "voyage") {
-            // <!-- Formulaire de choix de voyage -->
-            echo '<form method="post" action="recapitulatif.php">';
-            echo '<label for="choix-voyage">Choix voyage</label>';
+        <div class="panel-body">
+      <?php
+      if ($_SESSION['voyage-excursion'] == "voyage") {
+        // <!-- Formulaire de choix de voyage -->
+        echo '<form method="post" action="recapitulatif.php">';
+            echo '<label for="choix-voyage">Choix voyage : </label>';
             echo '<select name="choix-voyage" id="choix_voyage">';
             echo '<option value="Caraibes">Les caraibes - Le pied ! - 4500€ par personnes pour une semaine</option>';
             echo '<option value="Canaries">Les canaries - la main - 3500€ par personnes pour une semaine</option>';
             echo '</select><br>';
-            echo 'Nombres de personnes : <input type="number" name="nombre" placeholder="exemple : 3" required="requis" max="100">';
+            echo '<label>Nombres de personnes : </label><input type="number" name="nombre" placeholder="exemple : 3" required="requis" max="100">';
             echo '<br>';
             echo '<center><input type="submit" name="Estimation" value="Estimation"></center>';
-            echo '</form>';
-          }
-          else {
-            // <!-- Formulaire excursions -->
-            echo '<form method="post" action="recapitulatif.php">';
+        echo '</form>';
+
+      }
+      else {
+        // <!-- Formulaire excursions -->
+          echo '<form method="post" action="recapitulatif.php">';
             echo '<label for="Excursions">Choix d\'excursion :</label>';
-            echo '<select name="excursions" id="excursions">';
-            echo '<option value="Angleterre">Angleterre - 250€ par personne</option>';
-            echo '<option value="Espagne">Espagne - 200€ par personne</option>';
-            echo '</select>';
-            echo '<br>';
-            echo 'Nombre de personnes : <input type="number" name="nombre" placeholder="exemple : 3" required="requis" max="100">';
-            echo '<br>';
-            echo '<center><input type="submit" name="name" value="Estimation"></center>';
-            echo '</form>';
-          }
-        ?>
-      </div>
+              echo '<select name="excursions" id="excursions">';
+                echo '<option value="Angleterre">Angleterre - 250€ par personne</option>';
+                echo '<option value="Espagne">Espagne - 200€ par personne</option>';
+              echo '</select>';
+                echo '<br>';
+                echo '<label>Nombre de personnes : </label><input type="number" name="nombre" placeholder="exemple : 3" required="requis" max="100">';
+                echo '<br>';
+              echo '<center><input type="submit" name="name" value="Estimation"></center>';
+          echo '</form>';
+      }
+         ?>
+       </div>
     </div>
 
-   <!-- Bootstrap core JavaScript ================================================== -->
+
+
+   <!-- Bootstrap core JavaScript
+   ================================================== -->
    <!-- Placed at the end of the document so the pages load faster -->
    <!-- jquery libs -->
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
@@ -97,4 +101,4 @@
    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
    <script src="js/ie10-viewport-bug-workaround.js"></script>
  </body>
-</html>
+ </html>
